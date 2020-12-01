747e6256ec27923a3c8507052792b2c92afa86cc
TheInfiniter <dimonus98@yandex.ru>
2020-12-01 18:59:33 +0300

    111

--- Task-3/GaussLib/Gauss.cs
+++ Task-3/GaussLib/Gauss.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace GaussLib
 {
-    public class Gauss
+    public static class Gauss
     {
+        public static double[] GetGaussDome(int size, int amount, int amp, double disp, int center)
+        {
+            //RandomInit();
+            double[] array = new double[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 0;
+                for (int j = 0; j < amount; j++)
+                {
+                    array[i] += GaussDome(amp, disp, center, i);
+                }
+            }
+            return array;
+        }
+
+        private static double GaussDome(double amp, double disp, double center, int i, double shift = 0)
+        {
+            return amp * Math.Exp(-(i - (center + shift)) * (i - (center + shift)) / (disp * disp));
+        }
     }
 }
